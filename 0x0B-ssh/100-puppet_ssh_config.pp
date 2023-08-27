fb6737f2e5a7e1c49e5b5055c90addc27c75b0a2
stephenkiai <stephenkiai23@gmail.com>
2023-08-28 02:22:41 +0300

    edit ssh config using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,16 @@
 # client confg file edit using puppet
-file {'/home/ubuntu/.ssh/config':
-     ensure => present,
-     content => "
-     	     Host *
-	     IdentityFile ~/.ssh/school
-	     PasswordAuthentication no
-	",
+include stdlib
+
+file_line { 'Ensure Password Authentication is Disabled':
+  	  ensure  => present,
+  	  path    => '/etc/ssh/ssh_config',
+  	  line    => 'PasswordAuthentication no',
+	  replace => true,
+}
+
+file_line { 'Specify Identity File for SSH':
+  	  ensure  => present,
+  	  path    => '/etc/ssh/ssh_config',
+  	  line    => 'IdentityFile ~/.ssh/school',
+  	  replace => true,
 }
